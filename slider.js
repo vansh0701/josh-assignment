ad8a06fe510c973edf51ec8e0351c939f1f9d00f
vansh0701 <68152625+vansh0701@users.noreply.github.com>
2020-09-21 18:36:36 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,16 @@
+var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    speed:'900',
+    loop:true,
+    controls:false,
+    lazyload: true,
+    lazyloadSelector: ".tns-lazy",
+    nav:true,
+    navPosition:'bottom',
+    navContainer:'.image_controler',
+    autoplay:false,
+    autoplayButton:false,
+
+  });
